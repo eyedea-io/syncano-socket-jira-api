4b9554caefe48cfbe0e46929b6b9f5cd6b521959
Jan Rochalski <joonny220@gmail.com>
2019-01-18 16:56:20 +0100

    Init socket

---
+++ src/createTask.ts
@@ -0,0 +1,58 @@
+import * as S from '@eyedea/syncano'
+import axios from 'axios'
+
+interface Fields {
+  name: string
+  description: string
+  type: string
+}
+
+interface Args {
+  project: string
+  issues: Fields[]
+}
+
+class Endpoint extends S.Endpoint {
+  async run(
+    {response}: S.Core,
+    {args, config}: S.Context<Args>
+  ) {
+    const {project, issues} = args
+
+    const res = await Promise.all(
+      issues.map(async ({name, description, type}) => {
+        const {data} = await axios.request({
+          method: 'POST',
+          url: `https://${config.DOMAIN_NAME}.atlassian.net/rest/api/2/issue`,
+          auth: {
+            username: config.USER_EMAIL,
+            password: config.USER_API_TOKEN,
+          },
+          data: {
+            fields: {
+              project: {
+                key: project,
+              },
+              summary: name,
+              description,
+              issuetype: {
+                name: type,
+              },
+            },
+          },
+        })
+
+        return data
+      })
+    )
+
+    response.json(res)
+  }
+
+  endpointDidCatch(err) {
+    console.warn(err)
+    this.syncano.response.json({message: err.message})
+  }
+}
+
+export default ctx => new Endpoint(ctx)
